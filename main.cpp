7feaee0c3a91f77cf85f96eb9b30310ed954399c
frostyfos <winata.nando@gmail.com>
2014-01-28 06:54:39 +0700

    bikin file base

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    printf("hello world \n");
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
